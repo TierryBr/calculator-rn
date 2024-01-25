0dd55109972a84a21381d72158a035a35613242e
Tierry Barros <tierry@MacBook-Air-de-Tierry.local>
2024-01-25 19:26:16 -0300

    Create calculator

--- App.tsx
+++ App.tsx
@@ -1,118 +1,233 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default function App() {
+  const [displayValue, setDisplayValue] = useState('0');
+  const [operator, setOperator] = useState(null);
+  const [firstValue, setFirstValue] = useState('');
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const handleNumberInput = num => {
+    if (displayValue === '0') {
+      setDisplayValue(num.toString());
+    } else {
+      setDisplayValue(displayValue + num);
+    }
+  };
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const handleOperatorInput = operator => {
+    setOperator(operator);
+    setFirstValue(displayValue);
+    setDisplayValue('0');
+  };
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const handleEqual = () => {
+    const num1 = parseFloat(firstValue);
+    const num2 = parseFloat(displayValue);
+
+    if (operator === '+') {
+      setDisplayValue((num1 + num2).toString());
+    } else if (operator === '-') {
+      setDisplayValue((num1 - num2).toString());
+    } else if (operator === '*') {
+      setDisplayValue((num1 * num2).toString());
+    } else if (operator === '/') {
+      setDisplayValue((num1 / num2).toString());
+    }
+
+    setOperator(null);
+    setFirstValue('');
+  };
+
+  // Function to handle clear button press
+  const handleClear = () => {
+    setDisplayValue('0');
+    setOperator(null);
+    setFirstValue('');
+  };
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <View style={styles.displayContainer}>
+        <Text style={styles.displayText}>{displayValue}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleNumberInput(7)}>
+            <Text style={styles.buttonText}>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleNumberInput(8)}>
+            <Text style={styles.buttonText}>8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleNumberInput(9)}>
+            <Text style={styles.buttonText}>9</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.button, styles.operatorButton]}
+            onPress={() => handleOperatorInput('/')}>
+            <Text style={[styles.buttonText, styles.operatorButtonText]}>
+              ÷
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleNumberInput(4)}>
+            <Text style={styles.buttonText}>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleNumberInput(5)}>
+            <Text style={styles.buttonText}>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleNumberInput(6)}>
+            <Text style={styles.buttonText}>6</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.button, styles.operatorButton]}
+            onPress={() => handleOperatorInput('*')}>
+            <Text style={[styles.buttonText, styles.operatorButtonText]}>
+              ×
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleNumberInput(1)}>
+            <Text style={styles.buttonText}>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleNumberInput(2)}>
+            <Text style={styles.buttonText}>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleNumberInput(3)}>
+            <Text style={styles.buttonText}>3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.button, styles.operatorButton]}
+            onPress={() => handleOperatorInput('-')}>
+            <Text style={[styles.buttonText, styles.operatorButtonText]}>
+              −
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={[styles.button, styles.zeroButton]}
+            onPress={() => handleNumberInput(0)}>
+            <Text style={[styles.buttonText, styles.zeroButtonText]}>0</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.button, styles.operatorButton]}
+            onPress={() => handleOperatorInput('+')}>
+            <Text style={[styles.buttonText, styles.operatorButtonText]}>
+              +
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.equalButton} onPress={handleEqual}>
+          <Text style={styles.equalButtonText}>=</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.clearButton} onPress={handleClear}>
+          <Text style={styles.clearButtonText}>C</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
+// Styles
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionTitle: {
+  displayContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    padding: 20,
+  },
+  displayText: {
+    fontSize: 48,
+    color: '#fff',
+  },
+  buttonContainer: {
+    flex: 3,
+    width: '90%',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  button: {
+    flex: 1,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#353434',
+    elevation: 3,
+    margin: 2,
+    padding: 15,
+  },
+  buttonText: {
+    fontSize: 34,
+    color: '#fff',
+  },
+  zeroButton: {
+    flex: 3,
+    paddingLeft: 35,
+    paddingRight: 35,
+  },
+  zeroButtonText: {
+    marginLeft: 10,
+  },
+  operatorButton: {
+    backgroundColor: '#353434',
+  },
+  operatorButtonText: {
+    color: '#ff9500',
+  },
+  equalButton: {
+    flex: 0.6,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ff9500',
+    elevation: 3,
+  },
+  equalButtonText: {
+    fontSize: 32,
+    color: '#fff',
+  },
+  clearButton: {
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#353434',
+    marginTop: 10,
+    elevation: 3,
+    padding: 10,
+    marginBottom: 100,
+  },
+  clearButtonText: {
     fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    color: '#fff',
   },
 });
-
-export default App;
